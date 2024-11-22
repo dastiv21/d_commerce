8be78082dc55529d48fafea9aa061ba902b52381
ayusuf@afexnigeria.com <ayusuf@afexnigeria.com>
2024-11-22 05:20:28 +0100

    added files

--- d_commerce/settings.py
+++ d_commerce/settings.py
@@ -75,14 +75,20 @@ WSGI_APPLICATION = "d_commerce.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     },
+#     'test': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'test_db.sqlite3',
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    },
-    'test': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'test_db.sqlite3',
+        'NAME': ':memory:',  # Use an in-memory database for faster tests
     }
 }
 
